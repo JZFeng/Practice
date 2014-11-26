7a04017e47839a4d111d62356d7e13c0ebd4db3a
matthewfjz <jzhfeng001@gmail.com>
2014-11-25 17:15:46 -0800

    cxCxZ

--- src/gui/GUIDemo.java
+++ src/gui/GUIDemo.java
@@ -77,7 +77,7 @@ public class GUIDemo
 //        f.addWindowListener(new MyWin());
 
         f.setVisible(true);
-        System.out.println("Hello world!");
+        System.out.println("Hello Java World!");
     }
 
 }
