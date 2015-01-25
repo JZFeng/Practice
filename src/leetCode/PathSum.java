924c052575e0c33be676b62018e6a4128008bf72
matthewfjz <jzhfeng001@gmail.com>
2015-01-25 14:27:09 -0800

    s

---
+++ src/leetCode/PathSum.java
@@ -0,0 +1,38 @@
+package leetCode;
+
+public class PathSum
+{
+    public static void main(String args[])
+    {
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        System.out.println(hasPathSum(root, 1));
+    }
+
+    public static boolean hasPathSum(TreeNode root, int sum)
+    {
+        if (root == null)
+            return false;
+
+        if (root.left == null && root.right == null)
+            return (root.val == sum);
+        else
+            return (hasPathSum(root.left, sum - root.val) || (hasPathSum(root.right, sum - root.val)));
+
+    }
+
+    public static class TreeNode
+    {
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode(int i)
+        {
+            val = i;
+        }
+    }
+}
