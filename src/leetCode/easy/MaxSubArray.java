72c7d9268f421f0380d7e84f4a599812448d78be
jzhfeng <jzhfeng@JZHFENG-T430.telenav.com>
2015-06-22 08:10:07 -0700

    s

--- src/leetCode/easy/MaxSubArray.java
+++ src/leetCode/easy/MaxSubArray.java
@@ -15,36 +15,10 @@ public class MaxSubArray
 
         int[] a = {-2,1,3,5,-7};
         
-        System.out.println(maxSubArray1(a));
+        System.out.println(maxSubArray(a));
     }
 
-    public static int maxSubArray1(int[] a)
-    {
-        if(a == null || a.length ==0)
-            throw new IllegalArgumentException();
-        
-        int maxSum = a[0], curSum = a[0];
-        int start = 0, end = 0;
-        for (int i = 1; i < a.length; ++i)
-        {
-            curSum += a[i];
-            if (curSum < a[i])
-            {
-                curSum = a[i];
-                start = i;
-            }
-
-            if (curSum > maxSum)
-            {
-                maxSum = curSum;
-                end = i;
-            }
-        }
-        System.out.println("start index " + start);
-        System.out.println("end index " + end);
-        return maxSum;
-    }
-
+  
     public static int maxSubArray(int[] a)
     {
         if(a == null || a.length ==0)
