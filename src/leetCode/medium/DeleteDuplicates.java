04133640eb9d19b4c219b6bf75be6cc80f0d9ce0
jzhfeng <jzhfeng@JZHFENG-T430.telenav.com>
2015-06-24 08:38:22 -0700

    s

--- src/leetCode/medium/DeleteDuplicates.java
+++ src/leetCode/medium/DeleteDuplicates.java
@@ -1,6 +1,15 @@
 package leetCode.medium;
 
 import leetCode.utility.ListNode;
+/*
+Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+For example,
+Given 1->2->3->3->4->4->5, return 1->2->5.
+Given 1->1->1->2->3, return 2->3.
+ 
+ 其实道理还是一样，只是现在要把前驱指针指向上一个不重复的元素中，如果找到不重复元素，则把前驱指针知道该元素，否则删除此元素。算法只需要一遍扫描，时间复杂度是O(n)，空间只需要几个辅助指针，是O(1)。
+ */
 
 public class DeleteDuplicates
 {
