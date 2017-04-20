870852ca2ab4fcb88a008a85ad2bec07fc493c72
JZFeng <matthew.fjz@gmail.com>
2017-04-20 13:22:20 -0700

    Update DoubleLinkedList.java

--- src/dataStructure/doubleLinkedList/DoubleLinkedList.java
+++ src/dataStructure/doubleLinkedList/DoubleLinkedList.java
@@ -43,8 +43,8 @@ public class DoubleLinkedList implements Iterable
     public void addBefore(Dlnode p, Object x)
     {
         Dlnode newNode = new Dlnode(p.prev, x, p);
-        p.prev = newNode;
         newNode.prev.next = newNode;
+        p.prev = newNode;
         size++;
         modAmount++;
 
